9e8d25754022e1b52c8cb22e09e369235c97d73c
fukuda.takeshi <fukuda.takeshi@meta.co.jp>
2018-04-20 15:10:24 +0900

    ログインまで確認

--- src/main/java/org/docksidestage/app/application/security/SecurityConfig.java
+++ src/main/java/org/docksidestage/app/application/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.docksidestage.app.application.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,38 +21,45 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailService;
 
+    //private LoginService loginService;
+
+    //private UserDetailsService userDetailService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // #for_now for test, enable later by jflute
-        //http.authorizeRequests()
-        //        .antMatchers("/login", "/register")
-        //        .permitAll()
-        //        .antMatchers("/css/**", "/js/**", "/images/**")
-        //        .permitAll()
-        //        .antMatchers(HttpMethod.POST, "/login")
-        //        .permitAll()
-        //        .anyRequest()
-        //        .authenticated()
-        //        .and()
-        //        .formLogin()
-        //        .loginProcessingUrl("/login")
-        //        .defaultSuccessUrl("/member")
-        //        .failureUrl("/")
-        //        .usernameParameter("emailAddress")
-        //        .passwordParameter("password")
-        //        .permitAll()
-        //        .loginPage("/login")
-        //        .and()
-        //        .logout()
-        //        .logoutSuccessUrl("/login")
-        //        .permitAll()
-        //        .and()
-        //        .csrf()
-        //        .disable();
+        http.authorizeRequests()
+                .antMatchers("/login", "/register")
+                .permitAll()
+                .antMatchers("/css/**", "/js/**", "/images/**")
+                .permitAll()
+                .antMatchers(HttpMethod.POST, "/login")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/member")
+                .failureUrl("/login")
+                .usernameParameter("memberAccount")
+                .passwordParameter("password")
+                .permitAll()
+                .loginPage("/login")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login")
+                .permitAll()
+                .and()
+                .csrf()
+                .disable();
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailService).passwordEncoder(new StandardPasswordEncoder()); // 既存のsampleプロジェクトに合わせる
+        // 既存のsampleプロジェクトとはencode後の値が異なる。ser⇒5279bf7d2b9b3e8bccae7c1d8bb9a98577b78fd7dcbc8fa4fab72b72a54bf87318bc4453ca219bce
+        //auth.userDetailsService(userDetailService).passwordEncoder(new PlaintextPasswordEncoder());
+
     }
 }
--- src/test/java/org/docksidestage/app/web/login/LoginServiceTest.java
+++ src/test/java/org/docksidestage/app/web/login/LoginServiceTest.java
@@ -2,6 +2,8 @@ package org.docksidestage.app.web.login;
 
 import org.docksidestage.unit.UnitContainerTestCase;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 /**
  * @author jflute
@@ -20,4 +22,21 @@ public class LoginServiceTest extends UnitContainerTestCase {
         log(details);
         assertEquals("Pixy", details.getUsername());
     }
+
+    public void test_passwordEncoder() {
+        // ## Arrange ##
+        PasswordEncoder passwordEncoder = new StandardPasswordEncoder();
+
+        String password = "sea";
+        String digest = passwordEncoder.encode(password);
+        System.out.println("ハッシュ値 = " + digest);
+
+        if (passwordEncoder.matches(password, digest)) {
+            System.out.println("一致したよ");
+            return;
+        }
+        System.out.println("一致しなかったよ");
+
+    }
+
 }
